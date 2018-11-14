bb5a2f26c4c5aa97187e30ba49cfef3a20895e44
Sandra Cartas <ctr22c@gmail.com>
2018-11-14 11:04:33 +0100

    elimina archivos

--- agendaBasica/src/main/java/com/capgemini/agendaBasica/negocioAgenda/NegocioAgenda.java
+++ agendaBasica/src/main/java/com/capgemini/agendaBasica/negocioAgenda/NegocioAgenda.java
@@ -12,34 +12,34 @@ public class NegocioAgenda implements ServiciosNegocioAgenda{
 	
 	private List<ServiciosNegocioPersona> contactos;
  	
-	@Override
+	
 	public List<ServiciosNegocioPersona> getContactos() {
 		// TODO Auto-generated method stub
 		return this.contactos;
 	}
-	@Override
+	
 	public void setContactos(List<ServiciosNegocioPersona> contactos) {
 		// TODO Auto-generated method stub
 		this.contactos = contactos;
 	}
-	@Override
+	
 	public ServiciosNegocioPersona getContacto(String dni) {
 		// TODO Auto-generated method stub
 		ServiciosNegocioPersona contactoSelected = this.contactos.stream().filter(p -> p.getDni().contains(dni)).collect(Collectors.toList()).get(0);
 		return contactoSelected;
 	}
-	@Override
+	
 	public void setContacto(ServiciosNegocioPersona contacto) {
 		// TODO Auto-generated method stub
 		this.contactos.add(contacto);
  	}
-	@Override
+	
 	public void removeContacto(String dni) {
 		// TODO Auto-generated method stub
 		ServiciosNegocioPersona contactoDelete= this.contactos.stream().filter(p -> p.getDni().contains(dni)).collect(Collectors.toList()).get(0);
 		this.contactos.remove(contactoDelete);
 	}
-	@Override
+	
 	public void showContactos() {
 		// TODO Auto-generated method stub
 		
